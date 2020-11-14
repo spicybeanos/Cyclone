4c115f2d27f10779c13458abe6afe5c8124dbd53
spicybeanos <67200186+spicybeanos@users.noreply.github.com>
2020-11-14 12:10:09 +0530

    Add files via upload

---
+++ Cyclone2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using Cyclone.Lang;
+
+namespace Cyclone
+{
+    class Program
+    {
+        static CycloneCompiler compiler = new CycloneCompiler();
+        static void Main(string[] args)
+        {
+
+            string nl = Environment.NewLine;
+            
+            string[] test_ = CycloneCompiler.CompilerUtilities.GetGroupEncapsulatedDataWithChildren($"(hello world! {nl}(hello world)){nl}(io netty{nl}(io netty)){nl}(oof {nl}(oof))",'(',')',false);
+
+            foreach(string str_ in test_){
+                Console.WriteLine(str_);
+            }
+
+            if(args.Length > 0){
+                string path_ = args[0];
+                //string[] conts_ ;
+                if(File.Exists(path_) && (path_.EndsWith(".cyclone") || path_.EndsWith(".cy"))){
+                    //conts_   = File.ReadAllLines(path_);
+                    try{
+                        compiler.RunFile(path_);
+                    }
+                    catch(Exception e){
+                        CycloneCompiler.ConsoleFunctions.ThrowException($"Exception: {e}");
+                    }
+                    
+                }
+                else{
+                    CycloneCompiler.ConsoleFunctions.ThrowException($"{path_} is not a .cy or .cyclone file hence did not run it. \r\n Press enter to continue.");
+                    Console.ReadKey();
+                }
+            }
+            else{
+                CycloneCompiler.ConsoleFunctions.ThrowException("No file provided to run.Press any key to close.");
+                Console.ReadKey();
+            }                
+        }
+    }
+}
+
+
+/*
+
+            Console.ForegroundColor = ConsoleColor.White;
+            CycloneCompiler.ConsoleFunctions.print($"Running Cyclone version:{compiler.version}\n");
+            CycloneCompiler.ConsoleFunctions.Properties.Add("enable_debug","false");
+            string testString;
+            do
+            {
+                testString = Console.ReadLine();
+                compiler.Run(new string[]{testString});
+            } 
+            while (testString != ">end" && testString != ">exit" && testString != ">stop");
+            CycloneCompiler.ConsoleFunctions.print("Done.");
+
+*/
---
+++ Cyclone2/bin/Debug/netcoreapp3.1/test.cy
@@ -0,0 +1,44 @@
+func start
+{
+
+loop(5)
+print("Hello !!%n")
+
+f = 90.2f
+type_of(f)
+print("%n")
+
+//type_of(asa)
+
+str = "sa"
+type_of(str)
+print("%n")
+
+
+d<double> = 90.1
+type_of(d)
+print("%n")
+
+
+i<int> = 9
+type_of(i)
+
+
+print("%n")
+
+if(5 > 4)
+:c
+
+if(4 > 5)
+print("4 is greater than 5%n")
+
+print("Done. Press any enter to continue.%n")
+>pause
+}
+
+func c {
+print("5 is greater than 4%n")
+print("hello world!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%n")
+print("sdadssddassd dav   a%n")
+}
+
